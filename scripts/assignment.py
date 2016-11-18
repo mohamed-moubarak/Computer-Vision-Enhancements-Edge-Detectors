1ec82b2693db257f23cc0b521dd0105f98935c60
Mohamed Moubarak <mohamed.moubarak@student.guc.edu.eg>
2016-11-18 23:06:34 +0100

    submitted version

--- scripts/assignment.py
+++ scripts/assignment.py
@@ -3,6 +3,7 @@ import math
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
 
 def contrast_stretching(filepath):
@@ -38,10 +39,10 @@ def histogram_equalization(filepath):
 			if img[row, column] not in intensitiesList:
 				intensitiesList.extend([img[row, column]])
 
-	print 'Intensities in the image: ', str(intensitiesList)
+	# print 'Intensities in the image: ', str(intensitiesList)
 	
 	intensitiesList.sort()
-	print 'Sorted intensities in the image: ', str(intensitiesList)
+	# print 'Sorted intensities in the image: ', str(intensitiesList)
 
 	for intensity in intensitiesList:
 		count = 0
@@ -57,9 +58,9 @@ def histogram_equalization(filepath):
 			cumulativeValue = cumulativeFrequencyList[len(cumulativeFrequencyList)-1] + count
 			cumulativeFrequencyList.extend([cumulativeValue])
 
-	print 'Frequencies of each intensity (Sorted intensities): ', str(frequencyList)
+	# print 'Frequencies of each intensity (Sorted intensities): ', str(frequencyList)
 
-	print 'Cumulative frequencies: ', str(cumulativeFrequencyList)
+	# print 'Cumulative frequencies: ', str(cumulativeFrequencyList)
 
 	for row in xrange(0,img.shape[0]):
 		for column in xrange(0,img.shape[1]):
@@ -82,9 +83,34 @@ def sobel_edge_detection(filepath):
 			Gyimg[row - 1, column - 1] = math.fabs(Gy)
 			new[row - 1, column - 1] = math.fabs(Gx) + math.fabs(Gy)
 
+	for row in xrange(0,new.shape[0]):
+		for column in xrange(0,new.shape[1]):
+			if new[row, column] > 30:
+				new[row, column] = 255
+			else :
+				new[row, column] = 0
+
 	cv2.imwrite('..\\results\\sobel_edge_detection.png',new)
-	cv2.imwrite('..\\results\\sobel_edge_detection_Gx.png',Gximg)
-	cv2.imwrite('..\\results\\sobel_edge_detection_Gy.png',Gyimg)
+	# cv2.imwrite('..\\results\\sobel_edge_detection_Gx.png',Gximg)
+	# cv2.imwrite('..\\results\\sobel_edge_detection_Gy.png',Gyimg)
+
+def sobel_for_canny_edge_detection(filepath):
+	img = cv2.imread(filepath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	padded_image = cv2.copyMakeBorder(img,1,1,1,1,cv2.BORDER_CONSTANT,value = 0)
+	Gximg = img.copy()
+	Gyimg = img.copy()
+	new = img.copy()
+	for row in xrange(1,padded_image.shape[0]-1):
+		for column in xrange(1,padded_image.shape[1]-1):
+			Gx = (padded_image[row - 1, column + 1] + (2.0 * padded_image[row, column + 1]) + padded_image[row + 1, column + 1]) - (padded_image[row - 1, column - 1] + (2.0 * padded_image[row, column - 1]) + padded_image[row + 1, column - 1])
+			Gximg[row - 1, column - 1] = math.fabs(Gx)
+			Gy = (padded_image[row - 1, column - 1] + (2.0 * padded_image[row - 1, column]) + padded_image[row - 1, column + 1]) - (padded_image[row + 1, column - 1] + (2.0 * padded_image[row + 1, column]) + padded_image[row + 1, column + 1])
+			Gyimg[row - 1, column - 1] = math.fabs(Gy)
+			new[row - 1, column - 1] = math.fabs(Gx) + math.fabs(Gy)
+
+	return new
+
+	
 	
 def prewitt_edge_detection(filepath):
 	img = cv2.imread(filepath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
@@ -114,6 +140,13 @@ def prewitt_edge_detection(filepath):
 			kernelList.extend([math.fabs(kernel1), math.fabs(kernel2), math.fabs(kernel3), math.fabs(kernel4), math.fabs(kernel5), math.fabs(kernel6), math.fabs(kernel7), math.fabs(kernel8)])
 			new[row - 1, column - 1] = max(kernelList)
 
+	for row in xrange(0,new.shape[0]):
+		for column in xrange(0,new.shape[1]):
+			if new[row, column] > 20:
+				new[row, column] = 255
+			else :
+				new[row, column] = 0
+
 
 	cv2.imwrite('..\\results\\prewitt_edge_detection.png',new)
 
@@ -121,7 +154,7 @@ def canny_edge_detection(filepath, t1, t2):
 	img = cv2.imread(filepath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 	padded_image = cv2.copyMakeBorder(img,2,2,2,2,cv2.BORDER_CONSTANT,value = 0)
 	gaussian = img.copy()
-	canny = img.copy()
+	
 
 	for row in xrange(2,padded_image.shape[0]-2):
 		for column in xrange(2,padded_image.shape[1]-2):
@@ -129,21 +162,35 @@ def canny_edge_detection(filepath, t1, t2):
 
 			gaussian[row - 2, column - 2] = kernel / 272
 
-	# cv2.imwrite('..\\results\\gaussian_filter.png',gaussian)
+	cv2.imwrite('..\\results\\gaussian_filter.png',gaussian)
 
-	# prewitt_edge_detection('..\\results\\gaussian_filter.png')
-	# canny = cv2.imread('..\\results\\gaussian_filter.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	sobel_result = sobel_for_canny_edge_detection('..\\results\\gaussian_filter.png')
+
+	canny = sobel_result.copy()
+
+	for row in xrange(0,sobel_result.shape[0]):
+		for column in xrange(0,sobel_result.shape[1]):
+			if row == 0 or row == canny.shape[0] - 1 or column == 0 or column == canny.shape[1] - 1 or canny[row, column] < t2:
+				canny[row, column] = 0
+			if sobel_result[row, column] > t1:
+					canny[row, column] = 255
+			else :
+				if sobel_result[row, column] < t2:
+					canny[row, column] = 0
+
+				
+					
 
 	for row in xrange(0,canny.shape[0]):
 		for column in xrange(0,canny.shape[1]):
-			if row == 0 or row == canny.shape[0] - 1 or column == 0 or column == canny.shape[1] - 1 or canny[row, column] < t2:
-				canny[row, column] = 0
-			else:
-				if not (canny[row - 1, column - 1] > 0 or canny[row - 1, column] > 0 or canny[row - 1, column + 1] > 0 or canny[row, column - 1] > 0 or canny[row, column + 1] > 0 or canny[row + 1, column - 1] > 0 or canny[row + 1, column] > 0 or canny[row + 1, column + 1] > 0):
+			if canny[row, column] != 0 and canny[row, column] != 255:
+				# print canny[row,column]
+				if canny[row - 1, column - 1] == 255 or canny[row - 1, column] == 255 or canny[row - 1, column + 1] == 255 or canny[row, column - 1] == 255 or canny[row, column + 1] == 255 or canny[row + 1, column - 1] == 255 or canny[row + 1, column] == 255 or canny[row + 1, column + 1] == 255 :
+					canny[row, column] = 255
+				else :
 					canny[row, column] = 0
 				
-
-	
+	os.remove("..\\results\\gaussian_filter.png")
 	cv2.imwrite('..\\results\\canny_edge_detection.png',canny)
 	
 
@@ -156,10 +203,4 @@ contrast_stretching(image1)
 histogram_equalization(image1)
 sobel_edge_detection(image2)
 prewitt_edge_detection(image2)
-canny_edge_detection(image2, 150, 75)
-
-# print 'RGB shape: ', img.shape 
-# print 'rows: ', img.shape[0]
-# print 'columns: ', img.shape[1]
-# # print 'channels: ', img1.shape[2]
-# print 'Pixel intensity: ', img[240, 240]		
+canny_edge_detection(image2, 40, 20)
